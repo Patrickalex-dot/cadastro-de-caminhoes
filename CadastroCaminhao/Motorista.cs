08eed9ee48d0535936857ecca2c50633ba09b224
Patrick Alexandre <palexandre@live.com>
2022-06-26 21:08:47 -0300

    Mudei a versão do framework... recriei o projeto

---
+++ CadastroCaminhao/Motorista.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CadastroCaminhao
+{
+    public class Motorista
+    {
+        string Nome { get; set; }
+        string Endereco { get; set; }
+        int id { get; set; }
+    }
+}
---
+++ CadastroCaminhao/Viagem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CadastroCaminhao
+{
+    public class Viagem
+    {
+        Caminhao Caminhao { get; set; }
+        Motorista Motorista  { get; set; }
+        int id { get; set; }
+    }
+}
